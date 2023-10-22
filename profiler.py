e4c8457fd6cb1033fd4e26d8a7678e5883ac2a59
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-10-22 01:16:37 -0400

    added a more realistic function to profile, and now it works a lot better. Also added a parameter to specify what functions to trace

--- profiler.py
+++ profiler.py
@@ -4,7 +4,7 @@ import traceback
 import linecache
 
 class Profiler:
-	def __init__(self, test_timing=False, timing_isolation=False):
+	def __init__(self, test_timing=False, timing_isolation=False, timed_functions=None):
 		self.function_timings = {}
 		self.function_stack = []
 		self.function_runtime_overhead = [0]
@@ -13,6 +13,9 @@ class Profiler:
 		self.test_timing = test_timing
 		self.timing_isolation = timing_isolation
 
+		self.timing_functions = []
+		if timed_functions is not None:
+			self.timing_functions = timed_functions
 		beg = time.perf_counter()
 
 		for _ in range(5000):
@@ -52,43 +55,50 @@ class Profiler:
 
 	def function_timing(self, frame, event, arg, s_time):
 		if event == "call":
-			func_name = frame.f_code.co_name
-
-			#if we're timing isolation, then add the time of when current function gets paused 
-			#to the function stack
-			if self.timing_isolation and len(self.function_stack) > 0:
-				self.function_stack[-1][2] = time.perf_counter()
-
-			#add the new function information to the function_stack
-			self.function_stack.append([func_name, time.perf_counter(), 0, 0])
-			#add the current runtime to the overhead stack
-			self.function_runtime_overhead.append(0)
+			self.function_call_timing(frame)
 
 		elif event == "return":
-			prev = self.function_stack[-1]
-			temp = self.function_runtime_overhead[-1]
+			self.function_return_timing()
+		
+		#adds the overhead of calling "function_timing" to the overhead
+		self.function_runtime_overhead[-1] += (time.perf_counter() - s_time) * \
+			1950 + self.calibration_count
 
-			#if timing isolation and there was a function before it
-			if not self.timing_isolation and len(self.function_runtime_overhead) > 1:
-				#adds the current_overhead to the parent timing overhead
-				self.function_runtime_overhead[-2] += self.function_runtime_overhead[-1]
-				self.function_runtime_overhead.pop(-1)
+	def function_call_timing(self, frame):
+		func_name = frame.f_code.co_name
 
-			timestamp = time.perf_counter()
-			total_time = (timestamp - prev[1]) * 1000 - temp
+		#if we're timing isolation, then add the time of when current function gets paused 
+		#to the function stack
+		if self.timing_isolation and len(self.function_stack) > 0:
+			self.function_stack[-1][2] = time.perf_counter()
 
-			#adds the current funtime runtime info to the dictionary of values
+		#add the new function information to the function_stack
+		self.function_stack.append([func_name, time.perf_counter(), 0, 0])
+		#add the current runtime to the overhead stack
+		self.function_runtime_overhead.append(0)
+
+	def function_return_timing(self):
+		prev = self.function_stack[-1]
+		temp = self.function_runtime_overhead[-1]
+
+		#if timing isolation and there was a function before it
+		if not self.timing_isolation and len(self.function_runtime_overhead) > 1:
+			#adds the current_overhead to the parent timing overhead
+			self.function_runtime_overhead[-2] += self.function_runtime_overhead[-1]
+			self.function_runtime_overhead.pop(-1)
+
+		timestamp = time.perf_counter()
+		total_time = (timestamp - prev[1]) * 1000 - temp
+
+		#adds the current funtime runtime info to the dictionary of values
+		if self.timing_functions == [] or self.function_stack[-1][0] in self.timing_functions:
 			if self.function_stack[-1][0] not in self.function_timings:
 				self.function_timings[self.function_stack[-1][0]] = total_time
 			else:
 				self.function_timings[self.function_stack[-1][0]] += total_time
 
-			#removes it from the current stack
-			self.function_stack.pop(-1)
-		
-		#adds the overhead of calling "function_timing" to the overhead
-		self.function_runtime_overhead[-1] += (time.perf_counter() - s_time) * \
-			2000 + self.calibration_count
+		#removes it from the current stack
+		self.function_stack.pop(-1)
 
 	def printFunctionTimings(self):
 		for key, value in self.function_timings.items():
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from profiler import Profiler
+import requests
+import re
+import json
 import time
 
 def looper():
@@ -18,6 +21,29 @@ def C():
     global temp, arr
     arr.append(temp[:])
 
+def getReport():
+    with open('reports.txt', 'r') as file:
+        for line in file:
+            contents = line
+            link1 = 'https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/' + str(contents)
+            response = requests.get(link1[:-1])
+            parseReport(response.text)
+
+
+def parseReport(report):
+    dict1 = json.loads(report)
+
+    first_name = dict1[0]['first_name']
+    last_name = dict1[0]['last_name']
+    date = dict1[0]['filing_date']
+    #print(first_name, last_name, date)
+
+    transactions = dict1[0]['transactions']
+    for x in range(len(transactions)):
+        #print(transactions[x])
+        pass
+
+
 temp = []
 arr = []
 for x in range(50):
@@ -27,18 +53,18 @@ def main():
     y = 1
     for x in range(y):
 
-        newProfiler = Profiler(test_timing=True)
+        newProfiler = Profiler(test_timing=True, timed_functions=['getReport', 'parseReport'])
         temp = []
 
         start1 = time.perf_counter()
-        newProfiler.run(looper)
+        newProfiler.run(getReport)
         end1 = time.perf_counter()
         end_profile = newProfiler.printFunctionTimings()
 
         #newProfiler.clear()
-
+        
         start = time.perf_counter()
-        looper()
+        getReport()
         end = time.perf_counter()
 
         
